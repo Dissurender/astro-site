c648658596d7403e6dad273ac52903fde0df2c51
Dissurender <rhynogg@gmail.com>
2022-09-15 08:50:17 -0500

    fix imports

--- src/components/widgets/bookshelf/Bookshelf.svelte
+++ src/components/widgets/bookshelf/Bookshelf.svelte
@@ -1,13 +1,15 @@
 <script>
   import Books from './Books.svelte';
   import AddBook from './AddBook.svelte';
-  import { isManage } from '@atom/manage';
-  import Button from '@src/widgets/bookshelf/Button.svelte';
+  import { isManage } from '@atom/manage.js';
+  import Button from '@lib/widgets/bookshelf/Button.svelte';
+
+  let toggle = $isManage;
 </script>
 
 <div class="w-full max-w-4xl mt-4 mx-auto">
   <Button />
-  {#if $isManage}
+  {#if { toggle }}
     <AddBook />
   {/if}
 
