c27f16dd9bc06771320bc61fb0aacbd5592c7a08
Dissurender <rhynogg@gmail.com>
2022-09-30 10:56:32 -0500

    removed bookshelf endpoint
    Bookshelf will be provided externally going forward

--- src/components/widgets/bookshelf/Button.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import { isManage } from '@atom/manage';
-</script>
-
-<div class="w-full mx-auto">
-  <button on:click={() => isManage.set(!$isManage)} class="btn hover:btn-accent">Manage</button>
-</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,16 +2,6 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: ({ colors }) => ({
-      lighter: '',
-      light: '',
-      bg: '',
-      dark: '',
-      darker: '',
-      fuchsia: colors.fuchsia,
-      cyan: colors.cyan,
-      emerald: colors.emerald,
-    }),
     extend: {},
   },
   darkMode: 'class',
