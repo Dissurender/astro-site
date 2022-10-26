2ee4f1b11e8a1ef5b9f8f31695810a3655ada6b2
Dissurender <rhynogg@gmail.com>
2022-10-26 18:15:10 -0500

    footer rearranged

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import { SITE } from './src/config.mjs';
 import svelte from '@astrojs/svelte';
+import solidJs from "@astrojs/solid-js";
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  integrations: [svelte(), tailwind(), solidJs()],
   site: SITE.origin,
   base: SITE.basePathname,
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
